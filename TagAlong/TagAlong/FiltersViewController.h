a86ffece85cff9b131f846fdb64114a6f2de4054
Tushar <tsinghal@usc.edu>
2016-12-09 02:18:06 -0800

    filter picker screen added

---
+++ TagAlong/TagAlong/FiltersViewController.h
@@ -0,0 +1,13 @@
+//
+//  FiltersViewController.h
+//  TagAlong
+//
+//  Created by Tushar Singhal on 12/8/16.
+//  Copyright © 2016 Tushar Singhal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FiltersViewController : UIViewController
+
+@end
---
+++ TagAlong/TagAlong/FiltersViewController.m
@@ -0,0 +1,37 @@
+//
+//  FiltersViewController.m
+//  TagAlong
+//
+//  Created by Tushar Singhal on 12/8/16.
+//  Copyright © 2016 Tushar Singhal. All rights reserved.
+//
+
+#import "FiltersViewController.h"
+
+@interface FiltersViewController ()
+
+@end
+
+@implementation FiltersViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
